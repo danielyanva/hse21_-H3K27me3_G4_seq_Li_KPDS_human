ce295117a19db5cce6b6ecb46907a140494b0d16
danielyanva <85527962+danielyanva@users.noreply.github.com>
2021-06-08 22:41:36 +0300

    Create filter_peaks.R

---
+++ scr/filter_peaks.R
@@ -0,0 +1,36 @@
+install.packages("dplyr")
+
+library(ggplot2)
+library(dplyr)
+# library(tidyr)   # replace_na
+# library(tibble)  # column_to_rownames
+
+###
+
+# NAME <- 'H3K27me3_HeLa-S3.ENCFF428MSN.hg19'
+# NAME <- 'H3K27me3_HeLa-S3.ENCFF584RYA.hg19'
+
+OUT_DIR <- 'C:/Users/danie/Documents/hell/Results'
+
+###
+
+bed_df <- read.delim(paste0('C:/Users/danie/Documents/hell/', NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+bed_df <- bed_df %>%
+  arrange(-len) %>%
+  filter(len < 50000)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('len_hist.', NAME, '.filtered.pdf'), path = OUT_DIR)
+
+bed_df %>%
+  select(-len) %>%
+  write.table(file='C:/Users/danie/Documents/hell/H3K27me3_HeLa-S3.ENCFF584RYA.hg38.filtered.bed',
+              col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
