676c386db431ad4bda116388a2f94ed62f9d8a6b
danielyanva <85527962+danielyanva@users.noreply.github.com>
2021-06-08 22:52:05 +0300

    Update filter_peaks.R

--- scr/filter_peaks.R
+++ scr/filter_peaks.R
@@ -8,8 +8,7 @@ library(dplyr)
 ###
 
 # NAME <- 'H3K27me3_HeLa-S3.ENCFF428MSN.hg19'
-# NAME <- 'H3K27me3_HeLa-S3.ENCFF584RYA.hg19'
-
+NAME <- 'H3K27me3_HeLa-S3.ENCFF584RYA.hg19'
 OUT_DIR <- 'C:/Users/danie/Documents/hell/Results'
 
 ###
@@ -19,18 +18,26 @@ colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
 bed_df$len <- bed_df$end - bed_df$start
 head(bed_df)
 
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('filter_peaks.', NAME, '.init.hist.pdf'), path = OUT_DIR)
+
+# Remove long peaks
 bed_df <- bed_df %>%
   arrange(-len) %>%
-  filter(len < 50000)
+  filter(len < 5000)
 
 ggplot(bed_df) +
   aes(x = len) +
   geom_histogram() +
   ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
   theme_bw()
-ggsave(paste0('len_hist.', NAME, '.filtered.pdf'), path = OUT_DIR)
+ggsave(paste0('filter_peaks.', NAME, '.filtered.hist.pdf'), path = OUT_DIR)
 
 bed_df %>%
   select(-len) %>%
-  write.table(file='C:/Users/danie/Documents/hell/H3K27me3_HeLa-S3.ENCFF584RYA.hg38.filtered.bed',
+  write.table(file=paste0('C:/Users/danie/Documents/hell/', NAME ,'.filtered.bed'),
               col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
